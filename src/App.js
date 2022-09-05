266bb3926fc66ad77fb99f10cf861bcc3e3aff65
robwatsongtr <rwatso@gmail.com>
2022-09-05 16:56:06 -0700

    made top bar

--- src/App.js
+++ src/App.js
@@ -2,8 +2,87 @@ import Topbar from './components/topbar/Topbar'
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Topbar/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      test<br/>
+      
     </div>
   );
 }
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import './topbar.css'
+import { NotificationsNone, Settings } from '@material-ui/icons';
 
 export default function Topbar() {
   return (
     <div>
-      Topbar
+      <div className="topbar">
+        <div className="topbarWrapper">
+          <div className="topLeft">
+            <span className="logo">Portfolio Feedback</span>
+          </div>
+          <div className="topRight">
+            <div className="topbarIconContainer">
+              <NotificationsNone />
+              <span className="topIconBadge">2</span>
+            </div>
+            <div className="topbarIconContainer">
+              <Settings />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
