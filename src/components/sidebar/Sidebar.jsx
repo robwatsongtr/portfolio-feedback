ad764187bbe89249daa992a335590a05bee793de
robwatsongtr <rwatso@gmail.com>
2022-09-07 11:40:13 -0700

    example sidebar is working

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -13,7 +13,7 @@ import {
   WorkOutline,
   Report,
 } from "@material-ui/icons";
-import { Link } from "react-router-dom";
+
 
 export default function Sidebar() {
   return (
@@ -24,12 +24,12 @@ export default function Sidebar() {
             <div className="sidebarMenu">
               <h3 className="sidebarTitle">Dashboard</h3>
               <ul className="sidebarList">
-                <Link to="/" className="link">
+                
                 <li className="sidebarListItem active">
                   <LineStyle className="sidebarIcon" />
                   Home
                 </li>
-                </Link>
+                
                 <li className="sidebarListItem">
                   <Timeline className="sidebarIcon" />
                   Analytics
@@ -44,18 +44,18 @@ export default function Sidebar() {
             <div className="sidebarMenu">
               <h3 className="sidebarTitle">Quick Menu</h3>
               <ul className="sidebarList">
-                <Link to="/users" className="link">
+                
                   <li className="sidebarListItem">
                     <PermIdentity className="sidebarIcon" />
                     Users
                   </li>
-                </Link>
-                <Link to="/products" className="link">
+                
+                
                   <li className="sidebarListItem">
                     <Storefront className="sidebarIcon" />
                     Products
                   </li>
-                </Link>
+                
                 <li className="sidebarListItem">
                   <AttachMoney className="sidebarIcon" />
                   Transactions
@@ -84,7 +84,7 @@ export default function Sidebar() {
                 </li>
               </ul>
             </div>
-            
+
             <div className="sidebarMenu">
               <h3 className="sidebarTitle">Staff</h3>
               <ul className="sidebarList">
