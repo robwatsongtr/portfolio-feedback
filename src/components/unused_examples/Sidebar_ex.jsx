f87c10b8b7edaf74b0afc7ba4c6cb77ce26a3a9a
robwatsongtr <rwatso@gmail.com>
2022-09-07 13:21:17 -0700

    topbar and sidebar are looking pretty good now

--- src/components/unused_examples/Sidebar_ex.jsx
+++ src/components/unused_examples/Sidebar_ex.jsx
@@ -67,7 +67,41 @@ export default function Sidebar() {
               </ul>
             </div>
 
-            
+            <div className="sidebarMenu">
+              <h3 className="sidebarTitle">Notifications</h3>
+              <ul className="sidebarList">
+                <li className="sidebarListItem">
+                  <MailOutline className="sidebarIcon" />
+                  Mail
+                </li>
+                <li className="sidebarListItem">
+                  <DynamicFeed className="sidebarIcon" />
+                  Feedback
+                </li>
+                <li className="sidebarListItem">
+                  <ChatBubbleOutline className="sidebarIcon" />
+                  Messages
+                </li>
+              </ul>
+            </div>
+
+            <div className="sidebarMenu">
+              <h3 className="sidebarTitle">Staff</h3>
+              <ul className="sidebarList">
+                <li className="sidebarListItem">
+                  <WorkOutline className="sidebarIcon" />
+                  Manage
+                </li>
+                <li className="sidebarListItem">
+                  <Timeline className="sidebarIcon" />
+                  Analytics
+                </li>
+                <li className="sidebarListItem">
+                  <Report className="sidebarIcon" />
+                  Reports
+                </li>
+              </ul>
+            </div>
 
       </div>
 
