33429ff8063070695706265167bfb6a89c9ada0c
robwatsongtr <rwatso@gmail.com>
2022-09-09 14:24:03 -0700

    adding titles and rows to table

--- src/components/widgetLg/WidgetLg.jsx
+++ src/components/widgetLg/WidgetLg.jsx
@@ -1,9 +1,34 @@
 import "./widgetLg.css" 
+import { ArrowDownward, ArrowUpward } from "@material-ui/icons"
 
 export default function WidgetLg() {
   return (
     <div className="widgetLg">
-      Large Widget
+      <h3 className="widgetLgTitle">
+        Fund Summary
+      </h3>
+      <table className="widgetLgTable">
+        <tr className="widgetLgTr">
+          <th className="widgetLgTh">Fund Name</th>
+          <th className="widgetLgTh">Submitted Portfolio</th>
+          <th className="widgetLgTh">Recommended Portfolio</th>
+          <th className="widgetLgTh">Difference</th>
+          <th className="widgetLgTh">Feedback</th>
+          <th className="widgetLgTh">Suggestions</th>
+        </tr>
+        <tr className="widgetLgTr">
+          <td className="widgetLgFund">
+            <span className="widgetLgFundName">JP Morgan 4744</span>
+          </td>
+          <td className="widgetLgSubmittedPortfolio">10%</td>
+          <td className="widgetLgRecommendedPortfolio">9%</td>
+          <td className="widgetLgDifference">-1</td>
+          <td className="widgetLgFeedback"> <ArrowUpward/> </td>
+          <td className="widgetLgSuggestions">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          </td>
+        </tr>
+      </table>
     </div>
   )
 }
