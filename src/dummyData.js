81d206e8e80c69767000c1e42f50261f08e07940
robwatsongtr <rwatso@gmail.com>
2022-09-09 13:39:46 -0700

    started large widget

---
+++ src/dummyData.js
@@ -0,0 +1,2 @@
+// dummyData.js
+
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,16 @@
 import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo"
 import CurrentUserInfo from "../../components/currentUserInfo/CurrentUserInfo"
 import "./home.css"
+import WidgetLg from "../../components/widgetLg/WidgetLg"
 
 export default function Home() {
   return (
     <div className="home">
       <CurrentUserInfo />
       <FeaturedInfo />
+      <div className="homeWidgets">
+        <WidgetLg />
+      </div>
     </div>
   )
 }
