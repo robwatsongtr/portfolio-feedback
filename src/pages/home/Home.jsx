125da968619f1b5bab765d66e45bf49e097e064e
robwatsongtr <rwatso@gmail.com>
2022-09-12 20:51:24 -0700

    fixed the width of the sidebar

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -10,7 +10,6 @@ export default function Home() {
       <CurrentUserInfo />
       <FeaturedInfo />
       <div className="homeWidgets">
-        
         <FundSummaryWidget />
       </div>
     </div>
