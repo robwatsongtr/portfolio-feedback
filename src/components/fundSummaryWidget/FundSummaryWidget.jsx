f504e03407be7c4db88892b0f36285c9c3c4ae38
robwatsongtr <rwatso@gmail.com>
2022-09-15 14:42:06 -0700

    added thumbs up and down icons, and open icon for suggestions

--- src/components/fundSummaryWidget/FundSummaryWidget.jsx
+++ src/components/fundSummaryWidget/FundSummaryWidget.jsx
@@ -1,12 +1,21 @@
 import "./fundSummaryWidget.css"
 import { DataGrid } from '@material-ui/data-grid';
+// import { makeStyles } from '@material-ui/styles';
+
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
+import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
+import ThumbDownOffAltIcon from '@mui/icons-material/ThumbDownOffAlt';
 
 const columns = [
-  { field: 'id', headerName: 'ID', width: 90, hide: true },
+  { 
+    field: 'id', 
+    headerName: 'ID', 
+    width: 90, 
+    hide: true },
   {
     field: 'fundName',
     headerName: 'Fund Name',
-    width: 180,
+    width: 220,
     editable: false,
   },
   {
@@ -34,13 +43,23 @@ const columns = [
     field: 'feedback',
     headerName: 'Feedback',
     width: 150,
+    renderCell: (params) => {
+      if(params.value === 'true') {
+        return <> <ThumbUpOffAltIcon /> </>
+      } else {
+        return <> <ThumbDownOffAltIcon /> </>
+      }
+    },
     align: 'center',
     editable: false,
   },
   {
     field: 'suggestions',
     headerName: 'Suggestions',
-    width: 250,
+    width: 400,
+    renderCell: (params) => {
+      return  <> <OpenInNewIcon /> {params.row.suggestions} </> 
+    },
     editable: false,
   },
 ];
@@ -53,7 +72,14 @@ const rows = [
     difference: '-1',
     recommendedPortfolio: '9%',
     feedback: 'true',
-    suggestions: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    suggestions: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+      eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+      laboris nisi ut aliquip ex ea commodo consequat. 
+      Duis aute irure dolor in reprehenderit in voluptate velit esse 
+      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat 
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit 
+      anim id est laborum.`
   },
   { 
     id: 2, 
@@ -61,7 +87,7 @@ const rows = [
     submittedPortfolio: '20%', 
     difference: '9',
     recommendedPortfolio: '29%',
-    feedback: 'true',
+    feedback: 'false',
     suggestions: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
   },
   { 
@@ -79,10 +105,83 @@ const rows = [
     submittedPortfolio: '0%', 
     difference: '30',
     recommendedPortfolio: '30%',
-    feedback: 'true',
+    feedback: 'false',
     suggestions: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
   },
+  { 
+    id: 5, 
+    fundName: 'JP Morgan 478', 
+    submittedPortfolio: '0%', 
+    difference: '30',
+    recommendedPortfolio: '30%',
+    feedback: 'false',
+    suggestions: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+  },
+  { 
+    id: 6, 
+    fundName: 'JP Morgan 478', 
+    submittedPortfolio: '0%', 
+    difference: '30',
+    recommendedPortfolio: '30%',
+    feedback: 'false',
+    suggestions: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+  },
+  { 
+    id: 7, 
+    fundName: 'JP Morgan 478', 
+    submittedPortfolio: '0%', 
+    difference: '30',
+    recommendedPortfolio: '30%',
+    feedback: 'false',
+    suggestions: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+  },
+  { 
+    id: 8, 
+    fundName: 'JP Morgan 478', 
+    submittedPortfolio: '0%', 
+    difference: '30',
+    recommendedPortfolio: '30%',
+    feedback: 'false',
+    suggestions: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+  },
+  { 
+    id: 9, 
+    fundName: 'JP Morgan 478', 
+    submittedPortfolio: '0%', 
+    difference: '30',
+    recommendedPortfolio: '30%',
+    feedback: 'false',
+    suggestions: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+  },
+  { 
+    id: 10, 
+    fundName: 'JP Morgan 478', 
+    submittedPortfolio: '0%', 
+    difference: '30',
+    recommendedPortfolio: '30%',
+    feedback: 'false',
+    suggestions: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+  },
+  { 
+    id: 11, 
+    fundName: 'JP Morgan 478', 
+    submittedPortfolio: '0%', 
+    difference: '30',
+    recommendedPortfolio: '30%',
+    feedback: 'false',
+    suggestions: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+  },
+
 ];
+
+// const useStyles = makeStyles({
+//   root: {
+//     '& .super-app-theme--header': {
+//       backgroundColor: 'rgba(255, 7, 0, 0.55)',
+//     },
+//   },
+// });
+
 
 
 export default function FundSummaryList() {
@@ -94,11 +193,10 @@ export default function FundSummaryList() {
       </h3>
  
       <div className="dataTable">
-        <div style={{ height: 400, width: '100%' }}>
+        <div style={{ height: 800, width: '100%' }}>
           <DataGrid
             rows={rows}
             columns={columns}
-            
             disableSelectionOnClick
           />
         </div>
